b671d414d1af280254e0756cf7bc03eb98f9079f
Austin Valle <austinvalle@gmail.com>
2023-03-20 09:30:02 -0400

    remove skip message

--- helper/resource/plan_check.go
+++ helper/resource/plan_check.go
@@ -2,7 +2,6 @@ package resource
 
 import (
 	"context"
-	"fmt"
 
 	tfjson "github.com/hashicorp/terraform-json"
 	"github.com/hashicorp/terraform-plugin-testing/internal/errorshim"
@@ -22,13 +21,6 @@ type CheckPlanRequest struct {
 // TODO: document
 type CheckPlanResponse struct {
 	Error error
-
-	// Skip, if non-empty, immediately skips further TestStep and defines a message
-	// to be included with a call to (*testing.T).Skip(), which is visible in the test output.
-	//
-	// If a state has been applied via this TestStep or a prior TestStep, the testing will still
-	// invoke Terraform to destroy that state before finalizing the skipped test result.
-	Skip string
 }
 
 func runPlanChecks(ctx context.Context, t testing.T, plan *tfjson.Plan, planChecks []PlanCheck) error {
@@ -40,9 +32,6 @@ func runPlanChecks(ctx context.Context, t testing.T, plan *tfjson.Plan, planChec
 		resp := CheckPlanResponse{}
 		planCheck.CheckPlan(ctx, CheckPlanRequest{Plan: plan}, &resp)
 
-		if resp.Skip != "" {
-			t.Skip(fmt.Sprintf("plan check forced test skip: %s", resp.Skip))
-		}
 		if resp.Error != nil {
 			// TODO: Once Go 1.20 is the minimum supported version for this module, replace with `errors.Join` function
 			// - https://github.com/hashicorp/terraform-plugin-testing/issues/99
--- helper/resource/plan_check_test.go
+++ helper/resource/plan_check_test.go
@@ -6,12 +6,10 @@ var _ PlanCheck = &planCheckSpy{}
 
 type planCheckSpy struct {
 	err    error
-	skip   string
 	called bool
 }
 
 func (s *planCheckSpy) CheckPlan(ctx context.Context, req CheckPlanRequest, resp *CheckPlanResponse) {
 	s.called = true
-	resp.Skip = s.skip
 	resp.Error = s.err
 }
