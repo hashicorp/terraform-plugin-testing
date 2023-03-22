dbacdcf2124d212e1fac8d9b2f12b945dae77d13
Austin Valle <austinvalle@gmail.com>
2023-03-22 10:14:55 -0400

    remove nest

--- plancheck/expect_resource_action.go
+++ plancheck/expect_resource_action.go
@@ -17,56 +17,58 @@ func (e expectResourceAction) CheckPlan(ctx context.Context, req CheckPlanReques
 	foundResource := false
 
 	for _, rc := range req.Plan.ResourceChanges {
-		if e.resourceAddress == rc.Address {
-			switch e.actionType {
-			case ResourceActionNoop:
-				if !rc.Change.Actions.NoOp() {
-					resp.Error = fmt.Errorf("'%s' - expected %s, got action(s): %v", rc.Address, e.actionType, rc.Change.Actions)
-					return
-				}
-			case ResourceActionCreate:
-				if !rc.Change.Actions.Create() {
-					resp.Error = fmt.Errorf("'%s' - expected %s, got action(s): %v", rc.Address, e.actionType, rc.Change.Actions)
-					return
-				}
-			case ResourceActionRead:
-				if !rc.Change.Actions.Read() {
-					resp.Error = fmt.Errorf("'%s' - expected %s, got action(s): %v", rc.Address, e.actionType, rc.Change.Actions)
-					return
-				}
-			case ResourceActionUpdate:
-				if !rc.Change.Actions.Update() {
-					resp.Error = fmt.Errorf("'%s' - expected %s, got action(s): %v", rc.Address, e.actionType, rc.Change.Actions)
-					return
-				}
-			case ResourceActionDestroy:
-				if !rc.Change.Actions.Delete() {
-					resp.Error = fmt.Errorf("'%s' - expected %s, got action(s): %v", rc.Address, e.actionType, rc.Change.Actions)
-					return
-				}
-			case ResourceActionDestroyBeforeCreate:
-				if !rc.Change.Actions.DestroyBeforeCreate() {
-					resp.Error = fmt.Errorf("'%s' - expected %s, got action(s): %v", rc.Address, e.actionType, rc.Change.Actions)
-					return
-				}
-			case ResourceActionCreateBeforeDestroy:
-				if !rc.Change.Actions.CreateBeforeDestroy() {
-					resp.Error = fmt.Errorf("'%s' - expected %s, got action(s): %v", rc.Address, e.actionType, rc.Change.Actions)
-					return
-				}
-			case ResourceActionReplace:
-				if !rc.Change.Actions.Replace() {
-					resp.Error = fmt.Errorf("%s - expected %s, got action(s): %v", rc.Address, e.actionType, rc.Change.Actions)
-					return
-				}
-			default:
-				resp.Error = fmt.Errorf("%s - unexpected ResourceActionType: %s", rc.Address, e.actionType)
+		if e.resourceAddress != rc.Address {
+			continue
+		}
+
+		switch e.actionType {
+		case ResourceActionNoop:
+			if !rc.Change.Actions.NoOp() {
+				resp.Error = fmt.Errorf("'%s' - expected %s, got action(s): %v", rc.Address, e.actionType, rc.Change.Actions)
 				return
 			}
-
-			foundResource = true
-			break
+		case ResourceActionCreate:
+			if !rc.Change.Actions.Create() {
+				resp.Error = fmt.Errorf("'%s' - expected %s, got action(s): %v", rc.Address, e.actionType, rc.Change.Actions)
+				return
+			}
+		case ResourceActionRead:
+			if !rc.Change.Actions.Read() {
+				resp.Error = fmt.Errorf("'%s' - expected %s, got action(s): %v", rc.Address, e.actionType, rc.Change.Actions)
+				return
+			}
+		case ResourceActionUpdate:
+			if !rc.Change.Actions.Update() {
+				resp.Error = fmt.Errorf("'%s' - expected %s, got action(s): %v", rc.Address, e.actionType, rc.Change.Actions)
+				return
+			}
+		case ResourceActionDestroy:
+			if !rc.Change.Actions.Delete() {
+				resp.Error = fmt.Errorf("'%s' - expected %s, got action(s): %v", rc.Address, e.actionType, rc.Change.Actions)
+				return
+			}
+		case ResourceActionDestroyBeforeCreate:
+			if !rc.Change.Actions.DestroyBeforeCreate() {
+				resp.Error = fmt.Errorf("'%s' - expected %s, got action(s): %v", rc.Address, e.actionType, rc.Change.Actions)
+				return
+			}
+		case ResourceActionCreateBeforeDestroy:
+			if !rc.Change.Actions.CreateBeforeDestroy() {
+				resp.Error = fmt.Errorf("'%s' - expected %s, got action(s): %v", rc.Address, e.actionType, rc.Change.Actions)
+				return
+			}
+		case ResourceActionReplace:
+			if !rc.Change.Actions.Replace() {
+				resp.Error = fmt.Errorf("%s - expected %s, got action(s): %v", rc.Address, e.actionType, rc.Change.Actions)
+				return
+			}
+		default:
+			resp.Error = fmt.Errorf("%s - unexpected ResourceActionType: %s", rc.Address, e.actionType)
+			return
 		}
+
+		foundResource = true
+		break
 	}
 
 	if !foundResource {
