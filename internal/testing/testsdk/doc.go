49f2f2244ad698c3e38b36ab5c92f2cac70cab8d
Brian Flad <bflad417@gmail.com>
2023-08-22 14:05:26 -0400

    internal/testing: Add copyright headers

--- internal/testing/testsdk/doc.go
+++ internal/testing/testsdk/doc.go
@@ -1,3 +1,6 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
 // Package testsdk provides a lightweight terraform-plugin-go SDK for
 // implementing unit testing within this Go module.
 package testsdk
--- internal/testing/testsdk/provider/doc.go
+++ internal/testing/testsdk/provider/doc.go
@@ -1,2 +1,5 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
 // Package provider provides testsdk handling of the provider concept.
 package provider
