a08f279582b19cd403d2858a8f1fae407b1ae4a4
Benjamin Bennett <ben.bennett@hashicorp.com>
2024-01-16 10:14:09 +0000

    Adding ExpectNoValueExists check to replace TestCheckNoResourceAttr

---
+++ statecheck/expect_no_value_exists.go
@@ -0,0 +1,68 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+package statecheck
+
+import (
+	"context"
+	"fmt"
+
+	tfjson "github.com/hashicorp/terraform-json"
+
+	"github.com/hashicorp/terraform-plugin-testing/tfjsonpath"
+)
+
+// Resource State Check
+var _ StateCheck = expectNoValueExists{}
+
+type expectNoValueExists struct {
+	resourceAddress string
+	attributePath   tfjsonpath.Path
+}
+
+// CheckState implements the state check logic.
+func (e expectNoValueExists) CheckState(ctx context.Context, req CheckStateRequest, resp *CheckStateResponse) {
+	var rc *tfjson.StateResource
+
+	if req.State == nil {
+		resp.Error = fmt.Errorf("state is nil")
+	}
+
+	if req.State.Values == nil {
+		resp.Error = fmt.Errorf("state does not contain any state values")
+	}
+
+	if req.State.Values.RootModule == nil {
+		resp.Error = fmt.Errorf("state does not contain a root module")
+	}
+
+	for _, resourceChange := range req.State.Values.RootModule.Resources {
+		if e.resourceAddress == resourceChange.Address {
+			rc = resourceChange
+
+			break
+		}
+	}
+
+	// Resource doesn't exist
+	if rc == nil {
+		return
+	}
+
+	_, err := tfjsonpath.Traverse(rc.AttributeValues, e.attributePath)
+
+	if err == nil {
+		resp.Error = fmt.Errorf("attribute found at path: %s.%s", e.resourceAddress, e.attributePath.String())
+
+		return
+	}
+}
+
+// ExpectNoValueExists returns a state check that asserts that the specified attribute at the given resource
+// does not exist.
+func ExpectNoValueExists(resourceAddress string, attributePath tfjsonpath.Path) StateCheck {
+	return expectNoValueExists{
+		resourceAddress: resourceAddress,
+		attributePath:   attributePath,
+	}
+}
---
+++ statecheck/expect_no_value_exists_test.go
@@ -0,0 +1,94 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+package statecheck_test
+
+import (
+	"regexp"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+
+	r "github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/statecheck"
+	"github.com/hashicorp/terraform-plugin-testing/tfjsonpath"
+)
+
+func TestExpectNoValueExists_CheckState_ResourceNotFound(t *testing.T) {
+	t.Parallel()
+
+	r.Test(t, r.TestCase{
+		ProviderFactories: map[string]func() (*schema.Provider, error){
+			"test": func() (*schema.Provider, error) { //nolint:unparam // required signature
+				return testProvider(), nil
+			},
+		},
+		Steps: []r.TestStep{
+			{
+				Config: `resource "test_resource" "one" {
+					bool_attribute = true
+				}
+				`,
+				ConfigStateChecks: r.ConfigStateChecks{
+					statecheck.ExpectNoValueExists(
+						"does_not_exist",
+						tfjsonpath.New("bool_attribute"),
+					),
+				},
+			},
+		},
+	})
+}
+
+func TestExpectNoValueExists_CheckState_AttributeNotFound(t *testing.T) {
+	t.Parallel()
+
+	r.Test(t, r.TestCase{
+		ProviderFactories: map[string]func() (*schema.Provider, error){
+			"test": func() (*schema.Provider, error) { //nolint:unparam // required signature
+				return testProvider(), nil
+			},
+		},
+		Steps: []r.TestStep{
+			{
+				Config: `resource "test_resource" "one" {
+					bool_attribute = true
+				}
+				`,
+				ConfigStateChecks: r.ConfigStateChecks{
+					statecheck.ExpectNoValueExists(
+						"test_resource.one",
+						tfjsonpath.New("does_not_exist"),
+					),
+				},
+			},
+		},
+	})
+}
+
+func TestExpectNoValueExists_CheckState_AttributeFound(t *testing.T) {
+	t.Parallel()
+
+	r.Test(t, r.TestCase{
+		ProviderFactories: map[string]func() (*schema.Provider, error){
+			"test": func() (*schema.Provider, error) { //nolint:unparam // required signature
+				return testProvider(), nil
+			},
+		},
+		Steps: []r.TestStep{
+			{
+				Config: `resource "test_resource" "one" {
+					bool_attribute = true
+				}
+				`,
+				ConfigStateChecks: r.ConfigStateChecks{
+					statecheck.ExpectNoValueExists(
+						"test_resource.one",
+						tfjsonpath.New("bool_attribute"),
+					),
+				},
+				ExpectError: regexp.MustCompile(`attribute found at path: test_resource\.one\.bool_attribute`),
+			},
+		},
+	})
+}
