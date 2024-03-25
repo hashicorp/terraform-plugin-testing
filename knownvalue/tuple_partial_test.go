758daeda84e19140724eac7c99085dbb24b8a7d1
Austin Valle <austinvalle@gmail.com>
2024-03-25 12:04:29 -0400

    knownvalue: Add `Tuple*` equivalents for exact, partial, and size checks

---
+++ knownvalue/tuple_partial_test.go
@@ -0,0 +1,138 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+package knownvalue_test
+
+import (
+	"encoding/json"
+	"fmt"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+
+	"github.com/hashicorp/terraform-plugin-testing/knownvalue"
+)
+
+func TestTuplePartial_CheckValue(t *testing.T) {
+	t.Parallel()
+
+	testCases := map[string]struct {
+		self          knownvalue.Check
+		other         any
+		expectedError error
+	}{
+		"zero-nil": {
+			self:          knownvalue.TuplePartial(map[int]knownvalue.Check{}),
+			expectedError: fmt.Errorf("expected []any value for TuplePartial check, got: <nil>"),
+		},
+		"zero-other": {
+			self:  knownvalue.TuplePartial(map[int]knownvalue.Check{}),
+			other: []any{}, // checking against the underlying value field zero-value
+		},
+		"nil": {
+			self: knownvalue.TuplePartial(map[int]knownvalue.Check{
+				0: knownvalue.Float64Exact(1.23),
+				2: knownvalue.StringExact("world"),
+				3: knownvalue.Bool(true),
+			}),
+			expectedError: fmt.Errorf("expected []any value for TuplePartial check, got: <nil>"),
+		},
+		"wrong-type": {
+			self: knownvalue.TuplePartial(map[int]knownvalue.Check{
+				0: knownvalue.Float64Exact(1.23),
+				2: knownvalue.StringExact("world"),
+				3: knownvalue.Bool(true),
+			}),
+			other:         1.234,
+			expectedError: fmt.Errorf("expected []any value for TuplePartial check, got: float64"),
+		},
+		"empty": {
+			self: knownvalue.TuplePartial(map[int]knownvalue.Check{
+				0: knownvalue.Float64Exact(1.23),
+				2: knownvalue.StringExact("world"),
+				3: knownvalue.Bool(true),
+			}),
+			other:         []any{},
+			expectedError: fmt.Errorf("missing element index 0 for TuplePartial check"),
+		},
+		"wrong-length": {
+			self: knownvalue.TuplePartial(map[int]knownvalue.Check{
+				0: knownvalue.Float64Exact(1.23),
+				2: knownvalue.StringExact("world"),
+				3: knownvalue.Bool(true),
+			}),
+			other: []any{
+				json.Number("1.23"),
+				"hello",
+			},
+			expectedError: fmt.Errorf("missing element index 2 for TuplePartial check"),
+		},
+		"not-equal": {
+			self: knownvalue.TuplePartial(map[int]knownvalue.Check{
+				0: knownvalue.Float64Exact(1.23),
+				2: knownvalue.StringExact("world"),
+				3: knownvalue.Bool(true),
+			}),
+			other: []any{
+				json.Number("1.23"),
+				"world",
+				"hello",
+			},
+			expectedError: fmt.Errorf("tuple element 2: expected value world for StringExact check, got: hello"),
+		},
+		"wrong-order": {
+			self: knownvalue.TuplePartial(map[int]knownvalue.Check{
+				0: knownvalue.Float64Exact(1.23),
+				2: knownvalue.StringExact("world"),
+				3: knownvalue.Bool(true),
+			}),
+			other: []any{
+				json.Number("1.23"),
+				"world",
+				true,
+			},
+			expectedError: fmt.Errorf("tuple element 2: expected string value for StringExact check, got: bool"),
+		},
+		"equal": {
+			self: knownvalue.TuplePartial(map[int]knownvalue.Check{
+				0: knownvalue.Float64Exact(1.23),
+				2: knownvalue.StringExact("world"),
+				3: knownvalue.Bool(true),
+			}),
+			other: []any{
+				json.Number("1.23"),
+				"hello",
+				"world",
+				true,
+			},
+		},
+	}
+
+	for name, testCase := range testCases {
+		name, testCase := name, testCase
+
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			got := testCase.self.CheckValue(testCase.other)
+
+			if diff := cmp.Diff(got, testCase.expectedError, equateErrorMessage); diff != "" {
+				t.Errorf("unexpected difference: %s", diff)
+			}
+		})
+	}
+}
+
+func TestTuplePartialPartial_String(t *testing.T) {
+	t.Parallel()
+
+	got := knownvalue.TuplePartial(map[int]knownvalue.Check{
+		0: knownvalue.Float64Exact(1.23),
+		2: knownvalue.StringExact("world"),
+		3: knownvalue.Bool(true),
+	}).String()
+
+	if diff := cmp.Diff(got, "[0:1.23 2:world 3:true]"); diff != "" {
+		t.Errorf("unexpected difference: %s", diff)
+	}
+}
---
+++ knownvalue/tuple_size_test.go
@@ -0,0 +1,87 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+package knownvalue_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+
+	"github.com/hashicorp/terraform-plugin-testing/knownvalue"
+)
+
+func TestTupleSizeExact_CheckValue(t *testing.T) {
+	t.Parallel()
+
+	testCases := map[string]struct {
+		self          knownvalue.Check
+		other         any
+		expectedError error
+	}{
+		"zero-nil": {
+			self:          knownvalue.TupleSizeExact(0),
+			expectedError: fmt.Errorf("expected []any value for TupleSizeExact check, got: <nil>"),
+		},
+		"zero-other": {
+			self:  knownvalue.TupleSizeExact(0),
+			other: []any{}, // checking against the underlying value field zero-value
+		},
+		"nil": {
+			self:          knownvalue.TupleSizeExact(3),
+			expectedError: fmt.Errorf("expected []any value for TupleSizeExact check, got: <nil>"),
+		},
+		"wrong-type": {
+			self:          knownvalue.TupleSizeExact(3),
+			other:         1.234,
+			expectedError: fmt.Errorf("expected []any value for TupleSizeExact check, got: float64"),
+		},
+		"empty": {
+			self:          knownvalue.TupleSizeExact(3),
+			other:         []any{},
+			expectedError: fmt.Errorf("expected 3 elements for TupleSizeExact check, got 0 elements"),
+		},
+		"wrong-length": {
+			self: knownvalue.TupleSizeExact(4),
+			other: []any{
+				int64(123),
+				"hello",
+				true,
+			},
+			expectedError: fmt.Errorf("expected 4 elements for TupleSizeExact check, got 3 elements"),
+		},
+		"equal": {
+			self: knownvalue.TupleSizeExact(3),
+			other: []any{
+				int64(123),
+				"hello",
+				true,
+			},
+		},
+	}
+
+	for name, testCase := range testCases {
+		name, testCase := name, testCase
+
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			got := testCase.self.CheckValue(testCase.other)
+
+			if diff := cmp.Diff(got, testCase.expectedError, equateErrorMessage); diff != "" {
+				t.Errorf("unexpected difference: %s", diff)
+			}
+		})
+	}
+}
+
+func TestTupleSizeExact_String(t *testing.T) {
+	t.Parallel()
+
+	got := knownvalue.TupleSizeExact(2).String()
+
+	if diff := cmp.Diff(got, "2"); diff != "" {
+		t.Errorf("unexpected difference: %s", diff)
+	}
+}
