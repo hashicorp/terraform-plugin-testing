2f0ab6889728c1dd474eb919c9160e3e92460431
Bruno Schaatsbergen <git@bschaatsbergen.com>
2025-01-14 10:31:14 +0100

    extract: add extract bool and string value functions
    This introduces a new state check that extracts an underlying attribute value by traversing the state with tfjsonpath using a specified path.

---
+++ statecheck/extract_string_value_test.go
@@ -0,0 +1,59 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+package statecheck_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+
+	r "github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/statecheck"
+	"github.com/hashicorp/terraform-plugin-testing/tfjsonpath"
+)
+
+func TestExtractStringValue_Basic(t *testing.T) {
+	t.Parallel()
+
+	// targetVar will be set to the extracted value.
+	var targetVar string
+
+	r.Test(t, r.TestCase{
+		ProviderFactories: map[string]func() (*schema.Provider, error){
+			"test": func() (*schema.Provider, error) { //nolint:unparam // required signature
+				return testProvider(), nil
+			},
+		},
+		Steps: []r.TestStep{
+			{
+				Config: `resource "test_resource" "one" {
+					string_attribute = "str"
+				}
+				`,
+				ConfigStateChecks: []statecheck.StateCheck{
+					statecheck.ExtractStringValue(
+						"test_resource.one",
+						tfjsonpath.New("string_attribute"),
+						&targetVar,
+					),
+				},
+			},
+		},
+	})
+
+	t.Run("CheckTargetVar", func(t *testing.T) {
+		if err := testAccAssertStringEquals("str", targetVar); err != nil {
+			t.Errorf("Error in testAccAssertBoolEquals: %v", err)
+		}
+	})
+}
+
+// testAccAssertStringEquals compares the expected and target string values.
+func testAccAssertStringEquals(expected string, targetVar string) error {
+	if targetVar != expected {
+		return fmt.Errorf("expected targetVar to be %v, got %v", expected, targetVar)
+	}
+	return nil
+}
