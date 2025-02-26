ae8070fe7c92b4e580dca54e0063e8d75d8c462a
Baraa Basata <bbasata@gmail.com>
2025-02-26 07:55:46 -0500

    red, but better

--- statecheck/test_provider_test.go
+++ statecheck/test_provider_test.go
@@ -6,6 +6,8 @@ package statecheck_test
 import (
 	"context"
 
+	"github.com/hashicorp/terraform-plugin-go/tfprotov6"
+	"github.com/hashicorp/terraform-plugin-go/tftypes"
 	"github.com/hashicorp/terraform-plugin-testing/internal/testing/testsdk/datasource"
 	"github.com/hashicorp/terraform-plugin-testing/internal/testing/testsdk/provider"
 	"github.com/hashicorp/terraform-plugin-testing/internal/testing/testsdk/resource"
@@ -15,23 +17,30 @@ type aTestProvider struct {
 }
 
 func (t aTestProvider) Configure(ctx context.Context, request provider.ConfigureRequest, response *provider.ConfigureResponse) {
-	//TODO implement me
-	panic("implement me")
 }
 
 func (t aTestProvider) DataSourcesMap() map[string]datasource.DataSource {
-	//TODO implement me
-	panic("implement me")
+	return nil
 }
 
 func (t aTestProvider) ResourcesMap() map[string]resource.Resource {
-	//TODO implement me
-	panic("implement me")
+	return map[string]resource.Resource{
+		"test_resource": &aTestResource{},
+	}
 }
 
 func (t aTestProvider) Schema(ctx context.Context, request provider.SchemaRequest, response *provider.SchemaResponse) {
-	//TODO implement me
-	panic("implement me")
+	schema := tfprotov6.Schema{
+		Block: &tfprotov6.SchemaBlock{
+			Attributes: []*tfprotov6.SchemaAttribute{
+				{
+					Name: "planet",
+					Type: tftypes.String,
+				},
+			},
+		},
+	}
+	response.Schema = &schema
 }
 
 func (t aTestProvider) Stop(ctx context.Context, request provider.StopRequest, response *provider.StopResponse) {
@@ -40,6 +49,4 @@ func (t aTestProvider) Stop(ctx context.Context, request provider.StopRequest, r
 }
 
 func (t aTestProvider) ValidateConfig(ctx context.Context, request provider.ValidateConfigRequest, response *provider.ValidateConfigResponse) {
-	//TODO implement me
-	panic("implement me")
 }
--- statecheck/test_resource_test.go
+++ statecheck/test_resource_test.go
@@ -5,54 +5,65 @@ package statecheck_test
 
 import (
 	"context"
+
+	"github.com/hashicorp/terraform-plugin-go/tfprotov6"
+	"github.com/hashicorp/terraform-plugin-go/tftypes"
 	"github.com/hashicorp/terraform-plugin-testing/internal/testing/testsdk/resource"
 )
 
-type testResource struct {}
+type aTestResource struct{}
 
-func (t testResource) Create(ctx context.Context, request resource.CreateRequest, response *resource.CreateResponse) {
+func (t aTestResource) Create(ctx context.Context, request resource.CreateRequest, response *resource.CreateResponse) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (t testResource) Delete(ctx context.Context, request resource.DeleteRequest, response *resource.DeleteResponse) {
+func (t aTestResource) Delete(ctx context.Context, request resource.DeleteRequest, response *resource.DeleteResponse) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (t testResource) ImportState(ctx context.Context, request resource.ImportStateRequest, response *resource.ImportStateResponse) {
+func (t aTestResource) ImportState(ctx context.Context, request resource.ImportStateRequest, response *resource.ImportStateResponse) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (t testResource) PlanChange(ctx context.Context, request resource.PlanChangeRequest, response *resource.PlanChangeResponse) {
+func (t aTestResource) PlanChange(ctx context.Context, request resource.PlanChangeRequest, response *resource.PlanChangeResponse) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (t testResource) Read(ctx context.Context, request resource.ReadRequest, response *resource.ReadResponse) {
+func (t aTestResource) Read(ctx context.Context, request resource.ReadRequest, response *resource.ReadResponse) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (t testResource) Schema(ctx context.Context, request resource.SchemaRequest, response *resource.SchemaResponse) {
+func (t aTestResource) Schema(ctx context.Context, request resource.SchemaRequest, response *resource.SchemaResponse) {
+	schema := tfprotov6.Schema{
+		Block: &tfprotov6.SchemaBlock{
+			Attributes: []*tfprotov6.SchemaAttribute{
+				{
+					Name:     "bool_attribute",
+					Type:     tftypes.Bool,
+					Computed: true,
+				},
+			},
+			Description:     "",
+			DescriptionKind: 0,
+		},
+	}
+	response.Schema = &schema
+}
+
+func (t aTestResource) Update(ctx context.Context, request resource.UpdateRequest, response *resource.UpdateResponse) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (t testResource) Update(ctx context.Context, request resource.UpdateRequest, response *resource.UpdateResponse) {
+func (t aTestResource) UpgradeState(ctx context.Context, request resource.UpgradeStateRequest, response *resource.UpgradeStateResponse) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (t testResource) UpgradeState(ctx context.Context, request resource.UpgradeStateRequest, response *resource.UpgradeStateResponse) {
-	//TODO implement me
-	panic("implement me")
+func (t aTestResource) ValidateConfig(ctx context.Context, request resource.ValidateConfigRequest, response *resource.ValidateConfigResponse) {
 }
-
-func (t testResource) ValidateConfig(ctx context.Context, request resource.ValidateConfigRequest, response *resource.ValidateConfigResponse) {
-	//TODO implement me
-	panic("implement me")
-}
-
-
