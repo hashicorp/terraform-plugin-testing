96ff574850135e88e64f2722d774ce2c4f6f8eae
Bruno Schaatsbergen <git@bschaatsbergen.com>
2025-03-15 06:15:43 +0900

    knownvalue: Introduce function checks for primitive types (#412)
    * extract: add extract bool and string value functions
    
    This introduces a new state check that extracts an underlying attribute value by traversing the state with tfjsonpath using a specified path.
    
    * extract: improve error message
    
    * extract: improve subtest name
    
    * knownvalue: add custom check functions for validation
    
    * knownvalue: address failing tests
    
    * knownvalue: remove redundant cast
    
    * docs: add knownvalue func check documentation
    
    * knownvalue: add whole number (integer) test
    
    * docs: correct spaces
    
    * docs: correct spaces
    
    * changie: add entry
    
    * remove copyloopvar
    
    It is unnecessary to copy loop variables because these variables have per-iteration scope instead of per-loop scope as it was in Go < 1.22.

---
+++ knownvalue/bool_func_test.go
@@ -0,0 +1,75 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+package knownvalue_test
+
+import (
+	"encoding/json"
+	"fmt"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+
+	"github.com/hashicorp/terraform-plugin-testing/knownvalue"
+)
+
+func TestBoolFunc_CheckValue(t *testing.T) {
+	t.Parallel()
+
+	testCases := map[string]struct {
+		self          knownvalue.Check
+		other         any
+		expectedError error
+	}{
+		"nil": {
+			self:          knownvalue.BoolFunc(func(bool) error { return nil }),
+			expectedError: fmt.Errorf("expected bool value for BoolFunc check, got: <nil>"),
+		},
+		"wrong-type": {
+			self:          knownvalue.BoolFunc(func(bool) error { return nil }),
+			other:         json.Number("1.234"),
+			expectedError: fmt.Errorf("expected bool value for BoolFunc check, got: json.Number"),
+		},
+		"failure": {
+			self: knownvalue.BoolFunc(func(b bool) error {
+				if b != true {
+					return fmt.Errorf("%t was not true", b)
+				}
+				return nil
+			}),
+			other:         false,
+			expectedError: fmt.Errorf("%t was not true", false),
+		},
+		"success": {
+			self: knownvalue.BoolFunc(func(b bool) error {
+				if b != true {
+					return fmt.Errorf("%t was not foo", b)
+				}
+				return nil
+			}),
+			other: true,
+		},
+	}
+
+	for name, testCase := range testCases {
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			got := testCase.self.CheckValue(testCase.other)
+
+			if diff := cmp.Diff(got, testCase.expectedError, equateErrorMessage); diff != "" {
+				t.Errorf("unexpected difference: %s", diff)
+			}
+		})
+	}
+}
+
+func TestBoolFunc_String(t *testing.T) {
+	t.Parallel()
+
+	got := knownvalue.BoolFunc(func(bool) error { return nil }).String()
+
+	if diff := cmp.Diff(got, "BoolFunc"); diff != "" {
+		t.Errorf("unexpected difference: %s", diff)
+	}
+}
