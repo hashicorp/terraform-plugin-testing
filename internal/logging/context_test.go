456fde3a3df8d9479bf6652cf9f0874cde99da48
Baraa Basata <bbasata@gmail.com>
2025-04-24 12:53:09 -0400

    Remove unused code from internal/logging

--- internal/logging/context_test.go
+++ internal/logging/context_test.go
@@ -15,43 +15,6 @@ import (
 	"github.com/hashicorp/terraform-plugin-testing/internal/logging"
 )
 
-func TestInitContext(t *testing.T) {
-	t.Parallel()
-
-	var output bytes.Buffer
-
-	ctx := tfsdklogtest.RootLogger(context.Background(), &output)
-
-	// Simulate root logger fields that would have been associated by
-	// terraform-plugin-go prior to the InitContext() call.
-	ctx = tfsdklog.SetField(ctx, "tf_rpc", "GetProviderSchema")
-	ctx = tfsdklog.SetField(ctx, "tf_req_id", "123-testing-123")
-
-	ctx = logging.InitContext(ctx)
-
-	logging.HelperSchemaTrace(ctx, "test message")
-
-	entries, err := tfsdklogtest.MultilineJSONDecode(&output)
-
-	if err != nil {
-		t.Fatalf("unable to read multiple line JSON: %s", err)
-	}
-
-	expectedEntries := []map[string]interface{}{
-		{
-			"@level":    "trace",
-			"@message":  "test message",
-			"@module":   "sdk.helper_schema",
-			"tf_rpc":    "GetProviderSchema",
-			"tf_req_id": "123-testing-123",
-		},
-	}
-
-	if diff := cmp.Diff(entries, expectedEntries); diff != "" {
-		t.Errorf("unexpected difference: %s", diff)
-	}
-}
-
 func TestTestNameContext(t *testing.T) {
 	t.Parallel()
 
--- internal/logging/environment_variables.go
+++ internal/logging/environment_variables.go
@@ -20,8 +20,4 @@ const (
 	// unset.
 	EnvTfLogSdkHelperResource = "TF_LOG_SDK_HELPER_RESOURCE"
 
-	// EnvTfLogSdkHelperSchema is an environment variable that sets the logging
-	// level of SDK helper/schema loggers. Infers root SDK logging level, if
-	// unset.
-	EnvTfLogSdkHelperSchema = "TF_LOG_SDK_HELPER_SCHEMA"
 )
