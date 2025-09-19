45b23affc357258a257e4b3360bffaeacea97110
Steph <steph@hashicorp.com>
2025-09-19 11:06:25 +0200

    forgot one, oops

--- querycheck/expect_known_value_test.go
+++ querycheck/expect_known_value_test.go
@@ -34,3 +34,30 @@ func TestExpectKnownValue(t *testing.T) {
 		},
 	})
 }
+
+// Let's add a test case that checks the failure scenario when the value is incorrect.
+func TestExpectKnownValue_ValueIncorrect(t *testing.T) {
+	t.Parallel()
+
+	r.UnitTest(t, r.TestCase{
+		TerraformVersionChecks: []tfversion.TerraformVersionCheck{
+			tfversion.SkipBelow(tfversion.Version1_14_0),
+		},
+		ProtoV6ProviderFactories: map[string]func() (tfprotov6.ProviderServer, error){
+			"examplecloud": providerserver.NewProviderServer(testprovider.Provider{
+				ListResources: map[string]testprovider.ListResource{
+
+					// TODO: define a simpler resource and list resource here or copy the `examplecloud_test.go` and `examplecloud_list_resource.go` files here for use
+
+					//"examplecloud_containerette": examplecloudListResource(),
+				},
+				Resources: map[string]testprovider.Resource{
+					//"examplecloud_containerette": examplecloudResource(),
+				},
+			}),
+		},
+		Steps: []r.TestStep{
+			// TODO
+		},
+	})
+}
