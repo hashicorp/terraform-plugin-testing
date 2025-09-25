1b91d14a50dea48dedc2aecd7f70254c464c1165
Rain <rainskwan@gmail.com>
2025-09-25 14:32:14 -0400

    Revert "Saving my place"
    This reverts commit ccc4965d1ad46a9d41e7585019f3acec4a4e57d8.

--- querycheck/expect_result_length_exact_test.go
+++ querycheck/expect_result_length_exact_test.go
@@ -1,33 +1,35 @@
 package querycheck
 
 import (
-	"testing"
-
 	"github.com/hashicorp/terraform-plugin-go/tfprotov6"
+	r "github.com/hashicorp/terraform-plugin-testing/helper/resource"
 	"github.com/hashicorp/terraform-plugin-testing/internal/testing/testprovider"
 	"github.com/hashicorp/terraform-plugin-testing/internal/testing/testsdk/providerserver"
 	"github.com/hashicorp/terraform-plugin-testing/tfversion"
+	"testing"
 )
 
 func TestResultLengthExact(t *testing.T) {
 	t.Parallel()
 
-	UnitTest(t, TestCase{
+	r.UnitTest(t, r.TestCase{
 		TerraformVersionChecks: []tfversion.TerraformVersionCheck{
 			tfversion.SkipBelow(tfversion.Version1_14_0),
 		},
 		ProtoV6ProviderFactories: map[string]func() (tfprotov6.ProviderServer, error){
 			"examplecloud": providerserver.NewProviderServer(testprovider.Provider{
 				ListResources: map[string]testprovider.ListResource{
+
 					// TODO: define a simpler resource and list resource here or copy the `examplecloud_test.go` and `examplecloud_list_resource.go` files here for use
-					"examplecloud_containerette": examplecloudListResource(),
+
+					//"examplecloud_containerette": examplecloudListResource(),
 				},
 				Resources: map[string]testprovider.Resource{
-					"examplecloud_containerette": examplecloudResource(),
+					//"examplecloud_containerette": examplecloudResource(),
 				},
 			}),
 		},
-		Steps: []TestStep{
+		Steps: []r.TestStep{
 			// TODO
 		},
 	})
@@ -37,22 +39,24 @@ func TestResultLengthExact(t *testing.T) {
 func TestResultLengthExact_WrongAmount(t *testing.T) {
 	t.Parallel()
 
-	UnitTest(t, TestCase{
+	r.UnitTest(t, r.TestCase{
 		TerraformVersionChecks: []tfversion.TerraformVersionCheck{
 			tfversion.SkipBelow(tfversion.Version1_14_0),
 		},
 		ProtoV6ProviderFactories: map[string]func() (tfprotov6.ProviderServer, error){
 			"examplecloud": providerserver.NewProviderServer(testprovider.Provider{
 				ListResources: map[string]testprovider.ListResource{
+
 					// TODO: define a simpler resource and list resource here or copy the `examplecloud_test.go` and `examplecloud_list_resource.go` files here for use
-					"examplecloud_containerette": examplecloudListResource(),
+
+					//"examplecloud_containerette": examplecloudListResource(),
 				},
 				Resources: map[string]testprovider.Resource{
-					"examplecloud_containerette": examplecloudResource(),
+					//"examplecloud_containerette": examplecloudResource(),
 				},
 			}),
 		},
-		Steps: []TestStep{
+		Steps: []r.TestStep{
 			// TODO
 		},
 	})
